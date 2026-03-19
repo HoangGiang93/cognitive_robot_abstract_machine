4c39c82045803f6dee5551efc7942143612db998
fmuehlis <fmuehlis@posteo.de>
2026-03-19 22:36:53 +0100

    refactor: make imports more consistent

--- robokudo/src/robokudo/descriptors/object_knowledge/object_knowledge_iai_kitchen20.py
+++ robokudo/src/robokudo/descriptors/object_knowledge/object_knowledge_iai_kitchen20.py
@@ -11,7 +11,6 @@ from semantic_digital_twin.spatial_types import HomogeneousTransformationMatrix
 from semantic_digital_twin.world_description.geometry import Color, Scale
 
 
-
 @dataclass
 class ObjectKnowledgeBase(BaseObjectKnowledgeBase):
     def __init__(self):
